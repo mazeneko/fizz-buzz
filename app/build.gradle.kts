ce5f63fa01a223e3a27b25cf9213c703bb50fc7f
混猫。 <46620955+mazeneko@users.noreply.github.com>
2023-06-08 21:33:19 +0900

    ロガーとしてslf4jとlogbackを導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,12 @@ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
 
 repositories { mavenCentral() }
 
-dependencies { testImplementation("org.junit.jupiter:junit-jupiter:5.9.1") }
+dependencies {
+  implementation("org.slf4j:slf4j-api:2.0.7")
+  implementation("ch.qos.logback:logback-core:1.4.7")
+  implementation("ch.qos.logback:logback-classic:1.4.7")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+}
 
 application { mainClass.set("mazeneko.fizzbuzz.App") }
 
