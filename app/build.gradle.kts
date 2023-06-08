a767c3d614668eedef72473eb524f223d45794f3
混猫。 <46620955+mazeneko@users.noreply.github.com>
2023-06-08 19:07:38 +0900

    editorconfigのセットアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,29 +7,29 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+  // Apply the application plugin to add support for building a CLI application in Java.
+  application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+  // Use JUnit Jupiter for testing.
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:31.1-jre")
+  // This dependency is used by the application.
+  implementation("com.google.guava:guava:31.1-jre")
 }
 
 application {
-    // Define the main class for the application.
-    mainClass.set("mazeneko.fizzbuzz.App")
+  // Define the main class for the application.
+  mainClass.set("mazeneko.fizzbuzz.App")
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+  // Use JUnit Platform for unit tests.
+  useJUnitPlatform()
 }
